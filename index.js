b8630cdd73d4f206e5342fa82634a8aa58d05713
Rayhatron <rayhatron@gmail.com>
2017-05-29 13:02:15 +0200

    Landing page setup and mongoose install

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('It works!');
+    res.sendfile('./views/index.html');
 });
 
 app.listen(PORT, () => {
